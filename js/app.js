5f4fae69a02c94b95e3a4e0982a56b4b84fc2398
ndouthett <nick.douthett@gmail.com>
2015-01-02 19:57:04 +1100

    initial

---
+++ js/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '1000px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '500px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+  $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-still').hide();   
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }   
+  })
+  .keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+  
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
